010f7247439eeff3553b31b8cf71ee614ab5e2f1
omerssalih <onsalih10@gmail.com>
2023-12-12 14:03:14 +0300

    finished

---
+++ src/main/java/com/folks/security/inmemory/controller/PrivateController.java
@@ -0,0 +1,28 @@
+package com.folks.security.inmemory.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/private")
+public class PrivateController {
+
+    @GetMapping
+    public String helloWorldPrivate(){
+        return "Hello world! from private endpoint";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String helloWorldUserPrivate(){
+        return "Hello world! from user private endpoint";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String helloWorldAdminPrivate(){
+        return "Hello world! from admin private endpoint";
+    }
+}
---
+++ src/main/java/com/folks/security/inmemory/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.folks.security.inmemory.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService users(){
+        UserDetails user1 = User.builder()
+                .username("osg")
+                .password(bCryptPasswordEncoder().encode("osg"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("omer")
+                .password(bCryptPasswordEncoder().encode("omer"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception{
+        security
+                .headers(x -> x.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(x -> x.requestMatchers("/public/**", "/auth/**").permitAll())
+                .authorizeHttpRequests(x -> x.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return security.build();
+    }
+}
